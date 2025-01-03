9355f9b2c0762cd83972e1889f8dba26132f9b64
Hakim <ghods.hakim@hotmail.com>
2025-01-03 09:49:23 +0330

    Add project files.

---
+++ src/Sortings/Program.cs
@@ -0,0 +1,13 @@
+﻿using Sortings;
+
+Console.WriteLine("Quick Sort Demo");
+
+var random = new Random();
+
+var numbers = Enumerable.Range(1, 100).OrderBy(x => random.Next()).ToList();
+var sortedNumbers = SortBy.QuickSort(numbers);
+
+for (int i = 0; i < numbers.Count; i++)
+{
+    Console.WriteLine($"[{i + 1}] --- {numbers[i]:000} --> {sortedNumbers[i]:000}");
+}
---
+++ src/Sortings/QuickSort.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sortings;
+public static class SortBy
+{
+
+    public static List<int> QuickSort(List<int> numbers)
+    {
+        if (numbers.Count <= 2)
+        {
+            return numbers;
+        }
+
+
+        var pivot = numbers.Last();
+
+        var smallerNumbers = new List<int>();
+        var largerNumbers = new List<int>();
+
+
+        foreach (var number in numbers)
+        {
+            if (pivot < number)
+            {
+                smallerNumbers.Add(number);
+            }
+            else if (pivot > number)
+            {
+                largerNumbers.Add(number);
+            }
+        }
+
+        var sortedNumbers = new List<int>();
+
+        sortedNumbers.AddRange(QuickSort(smallerNumbers));
+        sortedNumbers.Add(pivot);
+        sortedNumbers.AddRange(QuickSort(largerNumbers));
+
+        return sortedNumbers;
+    }
+}
